408848ba70103007cb3ace375e413a24853a5ed4
ninebelowzero <richardjohnsilk@gmail.com>
2015-12-14 12:18:23 +0000

    Generates multiple choice answers with Underscore.

--- js/controllers/GameController.js
+++ js/controllers/GameController.js
@@ -8,21 +8,47 @@ function GameController($scope, Game){
   $scope.levelLoaded = false;
   $scope.characters = [];
   $scope.selectedCharacter = null;
+  $scope.answerA = null;
+  $scope.answerB = null;
+  $scope.answerC = null;
+  $scope.answerD = null;
 
   Game.firstBatch(function(data) {
-    $scope.characters = data.characters;
-    getRandomCharacter();
+    $scope.characters        = data.characters;
+    $scope.selectedCharacter = getRandomCharacter();
+    getAnswers();
     $scope.levelLoaded = true;
 
     // do some game logic, now data has loaded...
   });
 
-  function getRandomCharacter() {
-    $scope.selectedCharacter = $scope.characters[Math.ceil(Math.random() * $scope.characters.length)].String;
+  function getAnswers(){
+
+    var answers = [
+      $scope.selectedCharacter.kMandarin,
+      getRandomCharacter().kMandarin,
+      getRandomCharacter().kMandarin,
+      getRandomCharacter().kMandarin
+    ]
+
+    answers = _.map(answers, function(answer){
+      return answer.toLowerCase();
+    });
+
+    answers = _.shuffle(answers);
+
+    $scope.answerA = answers[0];
+    $scope.answerB = answers[1];
+    $scope.answerC = answers[2];
+    $scope.answerD = answers[3];
+
+    console.log(answers);
   }
 
-  $scope.chooseCharacter = function() {
-    getRandomCharacter();
+
+  function getRandomCharacter() {
+    return $scope.characters[Math.ceil(Math.random() * $scope.characters.length)];
   }
+
 
 }
