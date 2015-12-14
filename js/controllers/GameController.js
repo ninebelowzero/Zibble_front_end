e45a540db2eae7ec8f7d79405d39eb6210e34885
ninebelowzero <richardjohnsilk@gmail.com>
2015-12-14 11:50:07 +0000

    Added Pure CSS and <span>s for answers.

--- js/controllers/GameController.js
+++ js/controllers/GameController.js
@@ -5,7 +5,24 @@ angular
 GameController.$inject = ['$scope', 'Game'];
 function GameController($scope, Game){
 
-  $scope.characters = Game.firstBatch();
-  
+  $scope.levelLoaded = false;
+  $scope.characters = [];
+  $scope.selectedCharacter = null;
+
+  Game.firstBatch(function(data) {
+    $scope.characters = data.characters;
+    getRandomCharacter();
+    $scope.levelLoaded = true;
+
+    // do some game logic, now data has loaded...
+  });
+
+  function getRandomCharacter() {
+    $scope.selectedCharacter = $scope.characters[Math.ceil(Math.random() * $scope.characters.length)].String;
+  }
+
+  $scope.chooseCharacter = function() {
+    getRandomCharacter();
+  }
 
 }
