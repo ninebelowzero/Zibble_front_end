1efd17677f73c7e77b7cee427aea5c3b924f030d
ninebelowzero <richardjohnsilk@gmail.com>
2015-12-16 18:37:49 +0000

    Removed glitchy jQuery swipes, used ngTouch instead. Works for left/right.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 angular
-  .module('zibble', ['angular-jwt', 'ngResource', 'ui.router'])
+  .module('zibble', ['angular-jwt', 'ngResource', 'ui.router', 'ngTouch'])
   .constant('API', 'https://zibble-back-end.herokuapp.com')
   .config(function($httpProvider){
     $httpProvider.interceptors.push('AuthInterceptor');
--- js/controllers/GameController.js
+++ js/controllers/GameController.js
@@ -2,8 +2,8 @@ angular
   .module('zibble')
   .controller('GameController', GameController);
 
-GameController.$inject = ['$scope', '$timeout', 'Game', 'RegexService'];
-function GameController($scope, $timeout, Game, RegexService){
+GameController.$inject = ['$scope', '$timeout', 'Game', 'RegexService', '$swipe'];
+function GameController($scope, $timeout, Game, RegexService, $swipe){
 
   $scope.rightOrWrong  = "neither";
   $scope.message       = null;
@@ -50,8 +50,21 @@ function GameController($scope, $timeout, Game, RegexService){
     }, 1000)
   }
 
+  ///////// SWIPES /////////////
+
+  $scope.swipeLeft = function(){
+    console.log("Swiped left.");
+    $scope.choose($scope.answers[1]);
+  }
+
+  $scope.swipeRight = function(){
+    console.log("Swiped right.");
+    $scope.choose($scope.answers[2]);
+  }
+
+  //////////////////////////////
+
   $scope.choose = function(choice){
-  console.log(choice);
   if ($scope.asking == "pinyin"){
     if (choice == RegexService.clean($scope.selectedCharacter.kMandarin)) {
       $scope.asking = "definition";
@@ -67,29 +80,6 @@ function GameController($scope, $timeout, Game, RegexService){
       }
     }
   }
-
-  /////////////////////////////
-  ////// SWIPE EVENTS /////////
-  /////////////////////////////
-  
-  $('#trackpad').on('swipeleft', function(){
-    console.log("Swiped left!");
-  })
- 
-  $('#trackpad').on('swiperight', function(){
-    console.log("Swiped right!");
-  })
- 
-  $('#trackpad').on('swipeup', function(){
-    console.log("Swiped up!");
-  })
- 
-  $('#trackpad').on('swipedown', function(){
-    console.log("Swiped down!");
-  })
-
-
-  /////////////////////////////
 
   function getDefinitionAnswers(){
     $scope.message = "Select the right definition.";
@@ -132,78 +122,5 @@ function GameController($scope, $timeout, Game, RegexService){
       }, 1000);
     }
   }
-
-  ////////////////////////////
-
-  (function(){
-    var supportTouch    = $.support.touch,
-        scrollEvent     = "touchmove scroll",
-        touchStartEvent = supportTouch ? "touchstart" : "mousedown",
-        touchStopEvent  = supportTouch ? "touchend" : "mouseup",
-        touchMoveEvent  = supportTouch ? "touchmove" : "mousemove";
-    $.event.special.swipeupdown = {
-      setup: function() {
-        var thisObject = this;
-        var $this = $(thisObject);
-        $this.bind(touchStartEvent, function(event) {
-          var data = event.originalEvent.touches ?
-              event.originalEvent.touches[ 0 ] :
-              event,
-              start = {
-                time: (new Date).getTime(),
-                coords: [ data.pageX, data.pageY ],
-                origin: $(event.target)
-              },
-              stop;
-
-          function moveHandler(event) {
-            if (!start) {
-              return;
-            }
-            var data = event.originalEvent.touches ?
-                event.originalEvent.touches[ 0 ] :
-                event;
-            stop = {
-              time: (new Date).getTime(),
-              coords: [ data.pageX, data.pageY ]
-            };
-
-            // prevent scrolling
-            if (Math.abs(start.coords[1] - stop.coords[1]) > 10) {
-              event.preventDefault();
-            }
-          }
-          $this
-            .bind(touchMoveEvent, moveHandler)
-            .one(touchStopEvent, function(event) {
-              $this.unbind(touchMoveEvent, moveHandler);
-              if (start && stop) {
-                if (stop.time - start.time < 1000 &&
-                    Math.abs(start.coords[1] - stop.coords[1]) > 30 &&
-                    Math.abs(start.coords[0] - stop.coords[0]) < 75
-                  ){
-                  start.origin
-                    .trigger("swipeupdown")
-                    .trigger(start.coords[1] > stop.coords[1] ? "swipeup" : "swipedown");
-                }
-              }
-            start = stop = undefined;
-          });
-        });
-      }
-    };
-    $.each({
-      swipedown: "swipeupdown",
-      swipeup: "swipeupdown"
-    }, function(event, sourceEvent){
-      $.event.special[event] = {
-        setup: function(){
-          $(this).bind(sourceEvent, $.noop);
-        }
-      };
-    });
-
-  })();
-
 
 }
