d67ebf1f36b2616bbeceba7aec2f123c58d47e62
ninebelowzero <richardjohnsilk@gmail.com>
2015-12-16 10:32:12 +0000

    Refactored a wee bit.

--- js/controllers/GameController.js
+++ js/controllers/GameController.js
@@ -8,15 +8,11 @@ angular
 GameController.$inject = ['$scope', '$timeout', 'Game', 'RegexService'];
 function GameController($scope, $timeout, Game, RegexService){
 
-  console.log(RegexService);
-
-  $scope.isCorrect = "n-a";
+  $scope.isCorrect     = "n-a";
   $scope.showingAnswer = false;
-
-  $scope.correct    = 0;
-  $scope.incorrect  = 0;
-
-  $scope.level = 1;
+  $scope.correct       = 0;
+  $scope.incorrect     = 0;
+  $scope.level         = 1;
 
   Game.get({ id: $scope.level }, function(data){
     $scope.characters = _.shuffle(data.characters);
@@ -29,47 +25,52 @@ function GameController($scope, $timeout, Game, RegexService){
         $scope.asking = "definition";
         getDefinitionAnswers();
       } else {
-        $scope.incorrect++;
-        $scope.isCorrect = "incorrect";
-        $scope.message = "Wrong!";
-        $scope.showingAnswer = true;
-        $timeout(getNextCharacter, 2000); 
+        wrongAnswer();
       }
     } else if ($scope.asking == "definition"){
       if (choice == $scope.selectedCharacter.kDefinition){
-        $scope.correct++;
-        $scope.isCorrect = "correct";
-        $scope.message = "Correct!";
-        $timeout(getNextCharacter, 2000); 
+        rightAnswer();
       } else {
-        $scope.incorrect++;
-        $scope.isCorrect = "incorrect";
-        $scope.message = "Wrong!";
-        $scope.showingAnswer = true;
-        $timeout(getNextCharacter, 2000); 
+        wrongAnswer();
       }
     }
   }
 
+  function rightAnswer(){
+    $scope.correct++;
+    $scope.isCorrect = "correct";
+    $scope.message = "Right!";
+    getNextCharacter();
+  }
+
+  function wrongAnswer(){
+    $scope.incorrect++;
+    $scope.isCorrect = "incorrect";
+    $scope.message = "Wrong!";
+    $scope.showingAnswer = true;
+    $scope.correctPinyin = RegexService.clean($scope.selectedCharacter.kMandarin);
+    getNextCharacter();
+  }
+
   function getNextCharacter(){
-    $scope.message = "";
-    $scope.showingAnswer = false;
-    if ($scope.characters.length == 0){
-      endOfLevel();
-    } else {
-      $scope.asking = "pinyin";
-      $scope.selectedCharacter = $scope.characters.shift();
-  
-      $scope.answers = [ $scope.selectedCharacter.kMandarin ]
-      _(3).times(function(){
-        $scope.answers.push(getRandomCharacter().kMandarin);
-      });
-      $scope.answers = _.shuffle($scope.answers);
-  
-      $scope.answers = _.map($scope.answers, function(answer){
-        return RegexService.clean(answer);
-      });
-    }
+    $timeout(function(){
+      $scope.message = "";
+      $scope.showingAnswer = false;
+      if ($scope.characters.length == 0){
+        endOfLevel();
+      } else {
+        $scope.asking = "pinyin";
+        $scope.selectedCharacter = $scope.characters.shift();     
+        $scope.answers = [ $scope.selectedCharacter.kMandarin ]
+        _(3).times(function(){
+          $scope.answers.push(getRandomCharacter().kMandarin);
+        });
+        $scope.answers = _.shuffle($scope.answers);      
+        $scope.answers = _.map($scope.answers, function(answer){
+          return RegexService.clean(answer);
+        });
+      }
+    }, 1000)
   }
 
   function getDefinitionAnswers(){
