797d75d64824fb32f7fd71b89de4868104e6eb4f
ninebelowzero <richardjohnsilk@gmail.com>
2015-12-17 10:57:01 +0000

    Added functionality to display blockers.

--- js/controllers/BlockersController.js
+++ js/controllers/BlockersController.js
@@ -6,13 +6,19 @@ BlockersController.$inject = ['$scope', 'User', 'TokenService'];
 function BlockersController($scope, User, TokenService){
 
   $scope.user = TokenService.getUser();
+  $scope.blockers = [];
 
-  $scope.getBlockers = function(){
-    User.getBlockers({ id: $scope.user._id }, function(blockers){
-      console.log(blockers);
+  function initialize(){
+    User.getBlockers({ id: $scope.user._id }, function(res){
+      console.log("res:", res);
+      console.log("res.blockers:", res.blockers);
+      res.blockers.forEach(function(blocker){
+        console.log("blocker:", blocker);
+        $scope.blockers.push(blocker);
+      });
     });
   }
+
+  initialize();
   
-
-
 }
